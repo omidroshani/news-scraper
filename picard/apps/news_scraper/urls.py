23d37a0933dbff970f58d98d662b0bd1c2ca3c3e
Omid Roshani <omid.roshani@outlook.com>
2020-09-06 13:47:30 +0430

    add articles endpoint

--- picard/apps/news_scraper/urls.py
+++ picard/apps/news_scraper/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import update_sources,get_latest_news,get_feature_extraction
+from .views import update_sources,get_latest_news,get_feature_extraction,get_all_articles
 
 
 urlpatterns = [
     path('sources', update_sources, name='sources'),
     path('latest-news', get_latest_news, name='get_latest_news'),
     path('feature', get_feature_extraction, name='get_feature_extraction'),
+    path('articles', get_all_articles, name='get_all_articles'),
     # path('hello', update_sources_2, name='extract_news_content'),
 ]
